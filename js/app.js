dc368b8e464f6c8dbfbe42ea1dc4f61e9f861c5a
David Luna <21610290@utgz.edu.mx>
2024-11-29 12:59:15 -0600

    20

--- js/app.js
+++ js/app.js
@@ -13,52 +13,75 @@ if (navigator.serviceWorker && (window.location.protocol === 'http:' || window.l
 }
 
 // Referencias de jQuery
-var titulo = $('#titulo');
-var nuevoBtn = $('#nuevo-btn');
-var salirBtn = $('#salir-btn');
-var cancelarBtn = $('#cancel-btn');
-var postBtn = $('#post-btn');
-var avatarSel = $('#seleccion');
-var timeline = $('#timeline');
 
-var modal = $('#modal');
+var titulo      = $('#titulo');
+var nuevoBtn    = $('#nuevo-btn');
+var salirBtn    = $('#salir-btn');
+var cancelarBtn = $('#cancel-btn');
+var postBtn     = $('#post-btn');
+var avatarSel   = $('#seleccion');
+var timeline    = $('#timeline');
+
+var modal       = $('#modal');
 var modalAvatar = $('#modal-avatar');
-var avatarBtns = $('.seleccion-avatar');
-var txtMensaje = $('#txtMensaje');
+var avatarBtns  = $('.seleccion-avatar');
+var txtMensaje  = $('#txtMensaje');
 
 // El usuario, contiene el ID del héroe seleccionado
-var usuario = null;
+var usuario;
 
-// Crear un nuevo mensaje y agregarlo al timeline
+postBtn.on('click', function() {
+    var mensaje = txtMensaje.val();
+    if (mensaje.trim() === '') {
+        cancelarBtn.click();
+        return;  // Evita crear un mensaje vacío
+    }
+
+    crearMensajeHTML(mensaje, usuario);
+});
+
+nuevoBtn.on('click', function() {
+    console.log("Botón de nuevo clickeado");
+    modal.removeClass('oculto');
+    modal.animate({ 
+        opacity: 1,
+        top: '50%' // Ajusta la posición del modal
+    }, 200);
+});
+
+
+// ===== Codigo de la aplicación
+
 function crearMensajeHTML(mensaje, personaje) {
-    // Plantilla del mensaje
-    var content = `
-        <li class="animated fadeIn fast">
-            <div class="avatar">
-                <img src="img/avatars/${personaje}.jpg">
+
+    var content =`
+    <li class="animated fadeIn fast">
+        <div class="avatar">
+            <img src="img/avatars/${ personaje }.jpg">
+        </div>
+        <div class="bubble-container">
+            <div class="bubble">
+                <h3>@${ personaje }</h3>
+                <br/>
+                ${ mensaje }
             </div>
-            <div class="bubble-container">
-                <div class="bubble">
-                    <h3>@${personaje}</h3>
-                    <br/>
-                    ${mensaje}
-                </div>
-                <div class="arrow"></div>
-            </div>
-        </li>
+            
+            <div class="arrow"></div>
+        </div>
+    </li>
     `;
 
-    // Agregar el mensaje al inicio del timeline
     timeline.prepend(content);
-
-    // Limpiar el campo de texto y cerrar el modal
-    txtMensaje.val('');
     cancelarBtn.click();
-}
 
-// Manejo de inicio de sesión
-function logIn(ingreso) {
-    if (ingreso) {
+} 
+
+
+
+// Globals
+function logIn( ingreso ) {
+
+    if ( ingreso ) {
         nuevoBtn.removeClass('oculto');
         salirBtn.removeClass('oculto');
         timeline.removeClass('oculto');
@@ -69,52 +92,61 @@ function logIn(ingreso) {
         salirBtn.addClass('oculto');
         timeline.addClass('oculto');
         avatarSel.removeClass('oculto');
+
         titulo.text('Seleccione Personaje');
+    
     }
+
 }
 
-// Selección de personaje
-avatarBtns.on('click', function () {
+
+// Seleccion de personaje
+avatarBtns.on('click', function() {
+
     usuario = $(this).data('user');
+
     titulo.text('@' + usuario);
+
     logIn(true);
+
 });
 
-// Botón de salir
-salirBtn.on('click', function () {
+// Boton de salir
+salirBtn.on('click', function() {
+
     logIn(false);
+
 });
 
-// Botón de nuevo mensaje
-nuevoBtn.on('click', function () {
+// Boton de nuevo mensaje
+nuevoBtn.on('click', function() {
     modal.removeClass('oculto');
-    modal.animate({
-        opacity: 1,
-        top: '50%' // Centrar el modal
-    }, 200);
+    modal.animate({ 
+        opacity: 1,  // Asegúrate de que se vea el modal
+        top: '50%'    // Coloca el modal en la posición centrada
+    }, 200 );
 });
 
-// Botón de cancelar mensaje
-cancelarBtn.on('click', function () {
-    modal.animate({
-        marginTop: '+=1000px',
-        opacity: 0
-    }, 200, function () {
+// Boton de cancelar mensaje
+cancelarBtn.on('click', function() {
+   modal.animate({ 
+       marginTop: '+=1000px',
+       opacity: 0
+    }, 200, function() {
         modal.addClass('oculto');
-        txtMensaje.val(''); // Limpiar el campo de texto
+        txtMensaje.val('');
     });
 });
 
-// Botón de enviar mensaje
-postBtn.off('click').on('click', function () {
-    var mensaje = txtMensaje.val().trim();
+// Boton de enviar mensaje
+postBtn.on('click', function() {
 
-    // Validar que el mensaje no esté vacío
-    if (mensaje === '') {
+    var mensaje = txtMensaje.val();
+    if ( mensaje.length === 0 ) {
         cancelarBtn.click();
         return;
     }
 
-    // Crear el mensaje en el timeline
-    crearMensajeHTML(mensaje, usuario);
+    crearMensajeHTML( mensaje, usuario );
+
 });
